d604f8ca03b63d4cc9e8fa966e36595326db545a
nadia <nadiageras.au@gmail.com>
2023-10-25 20:43:02 +1000

    add scrollSpy function

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,10 @@ $(function () {
   let header = $("#header");
   let introHeight = intro.innerHeight();
   let headerHeight = header.innerHeight();
+  let scrollTop = $(window).scrollTop();
+
+  /* Header class on scroll
+  ==============================================*/
 
   headerScroll();
 
@@ -14,12 +18,59 @@ $(function () {
     introHeight = intro.innerHeight();
     headerHeight = header.innerHeight();
 
-    let scrollTop = $(this).scrollTop();
+    scrollTop = $(this).scrollTop();
 
     if (scrollTop >= introHeight - headerHeight) {
       header.addClass("header--dark");
     } else {
       header.removeClass("header--dark");
     }
+  }
+
+  /* Smooth scroll to sections
+  =========================================================*/
+
+  $("[data-scroll]").on("click", function (event) {
+    event.preventDefault();
+
+    let scrollEl = $(this).data("scroll");
+    let scrollElPos =
+      $(scrollEl).offset().top; /* get position of block from the top */
+
+    $("html, body").animate(
+      {
+        scrollTop: scrollElPos - headerHeight,
+      },
+      500
+    );
+  });
+
+  /* ScrollSpy
+  ========================================================= */
+
+  let windowH = $(window).height();
+  scrollSpy(scrollTop);
+
+  $(window).on("scroll", function () {
+    let scrollTop = $(this).scrollTop();
+    scrollSpy(scrollTop);
+  });
+
+  function scrollSpy(scrollTop) {
+    $("[data-scrollspy]").each(function () {
+      let $this = $(this);
+      let sectionId = $(this).data("scrollspy");
+      let sectionOffset = $this.offset().top;
+      sectionOffset = sectionOffset - windowH * 0.25;
+
+      if (scrollTop >= sectionOffset) {
+        $("#nav [data-scroll]").removeClass("active");
+        $('#nav [data-scroll="' + sectionId + '"]').addClass("active");
+      }
+
+      if (scrollTop == 0) {
+        $("#nav [data-scroll]").removeClass("active");
+      }
+    });
   }
 });
