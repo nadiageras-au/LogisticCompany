c4707a7864134b8e7a4893bc22d99c802153ab0e
nadia <nadiageras.au@gmail.com>
2023-10-30 12:15:52 +1000

    finish

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,24 @@
 $(function () {
+  /*Nav Toggle on mobile - burger 
+  ==============================================*/
+
+  let navToggle = $("#navToggle");
+  let nav = $("#nav");
+
+  navToggle.on("click", function (event) {
+    event.preventDefault();
+
+    $("body").toggleClass("show-nav");
+    $(this).toggleClass("active");
+    nav.toggleClass("show");
+  });
+
+  $(window).on("resize", function () {
+    $("body").removeClass("show-nav");
+    navToggle.removeClass("active");
+    nav.removeClass("show");
+  });
+
   let intro = $("#intro");
   let header = $("#header");
   let introHeight = intro.innerHeight();
@@ -37,6 +57,10 @@ $(function () {
     let scrollElPos =
       $(scrollEl).offset().top; /* get position of block from the top */
 
+    $("body").removeClass("show-nav");
+    navToggle.removeClass("active");
+    nav.removeClass("show");
+
     $("html, body").animate(
       {
         scrollTop: scrollElPos - headerHeight,
@@ -73,4 +97,116 @@ $(function () {
       }
     });
   }
+
+  /* Modal
+  ========================================================= */
+  /* Open modals */
+  $("[data-modal]").on("click", function (event) {
+    event.preventDefault();
+
+    let modal = $(this).data("modal");
+
+    $("body").addClass("no-scroll");
+    $(modal).addClass("show");
+
+    setTimeout(function () {
+      $(modal).find(".modal__content").css({
+        transform: "scale(1)",
+        opacity: 1,
+      });
+    });
+  });
+
+  //======================================================
+  /* Close modals */
+  $("[data-modal-close]").on("click", function (event) {
+    event.preventDefault();
+    let modal = $(this).parents(".modal");
+
+    modalClose(modal);
+  });
+
+  $(".modal").on("click", function () {
+    let modal = $(this);
+
+    modalClose(modal);
+  });
+
+  $(".modal__content").on("click", function (event) {
+    event.stopPropagation();
+  });
+
+  function modalClose(modal) {
+    modal.find(".modal__content").css({
+      transform: "scale(0.5)",
+      opacity: 0,
+    });
+
+    setTimeout(function () {
+      modal.removeClass("show");
+      $("body").removeClass("no-scroll");
+    }, 200);
+  }
+
+  /* Slick slider 
+   https://kenwheeler.github.io/slick/
+  ========================================================= */
+
+  // Intro Slider
+  let introSlider = $("#introSlider");
+
+  introSlider.slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    speed: 1000,
+  });
+
+  $("#introSliderPrev").on("click", function () {
+    introSlider.slick("slickPrev");
+  });
+
+  $("#introSliderNext").on("click", function () {
+    introSlider.slick("slickNext");
+  });
+
+  // Reviews Slider
+  let reviewsSlider = $("#reviewsSlider");
+
+  reviewsSlider.slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    speed: 500,
+  });
+
+  /* AOS animation for fade blocks
+  https://michalsnik.github.io/aos/
+  ========================================================= */
+  AOS.init({
+    // Global settings:
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: "DOMContentLoaded", // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: "aos-init", // class applied after initialization
+    animatedClassName: "aos-animate", // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 400, // values from 0 to 3000, with step 50ms
+    easing: "ease", // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
+  });
 });
